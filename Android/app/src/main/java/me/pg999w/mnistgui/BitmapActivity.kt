64ff983bbb7c08c0a4126e9a2429ecbfc5d51816
Peng Guanwen <pg999w@outlook.com>
2019-05-23 19:27:21 +0800

    Add chart

--- Android/app/src/main/java/me/pg999w/mnistgui/BitmapActivity.kt
+++ Android/app/src/main/java/me/pg999w/mnistgui/BitmapActivity.kt
@@ -3,6 +3,8 @@ package me.pg999w.mnistgui
 import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anychart.AnyChart
+import com.anychart.chart.common.dataentry.ValueDataEntry
 import kotlinx.android.synthetic.main.activity_bitmap.*
 import me.pg999w.mnistgui.tflite.MnistClassifier
 
@@ -24,6 +26,15 @@ class BitmapActivity : AppCompatActivity() {
         val classifier = MnistClassifier(this, MnistClassifier.Device.CPU, 1)
         val result = classifier.use { it.recognizeImage(bitmap) }
         val maxIndex = result.mapIndexed { i, v -> Pair(i, v) }.maxBy { it.second }!!.first
+
+        // render text
         result_text.text = maxIndex.toString()
+
+        // draw chart
+        val column = AnyChart.column()
+        val dataEntry = result.mapIndexed { i, v -> ValueDataEntry(i, v) }
+        column.data(dataEntry)
+        column.yAxis(false)
+        prob_chart.setChart(column)
     }
 }
--- Android/app/src/main/java/me/pg999w/mnistgui/MainActivity.kt
+++ Android/app/src/main/java/me/pg999w/mnistgui/MainActivity.kt
@@ -2,13 +2,10 @@ package me.pg999w.mnistgui
 
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
 
